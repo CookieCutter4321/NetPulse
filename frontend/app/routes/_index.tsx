7dd471564ea2373b5aeae094ffda5acafb9695c5
CookieCutter4321 <65974620+CookieCutter4321@users.noreply.github.com>
2026-06-30 02:06:53 -0400

    login

---
+++ frontend/app/routes/_index.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/card"
+import { Input } from "@/components/input"
+import { Button } from "@/components/button"
+  
+function chat() {
+ function handleSend() {
+    return
+ }
+
+ 
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-50 p-4">
+        <Card className="flex h-125 w-125 flex-col shadow-lg bg-white">
+        <CardHeader>
+          <CardTitle className="text-lg font-semibold text-slate-800"></CardTitle>
+        </CardHeader>
+
+      </Card>
+    </div>
+  )
+
+}
+
+export default chat
